f97665b84920f8fcbb0724221cfe97f9d86a1c16
JeongSW123 <123189509+JeongSW123@users.noreply.github.com>
2026-05-03 01:34:22 +0900

    feat: templates파일 수정

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,6 @@ urlpatterns = [
   path("signup/", TemplateView.as_view(template_name="accounts/signup.html"), name="page-signup"),
   path("password-reset/", TemplateView.as_view(template_name="accounts/password_reset.html"), name="page-password-reset"),
   path("", TemplateView.as_view(template_name="index.html"), name="page-home"),
-  path("threads/", TemplateView.as_view(template_name="chat/thread_list.html"), name="page-thread-list"),
-  path("threads/<uuid:pk>/", TemplateView.as_view(template_name="chat/thread_detail.html"), name="page-thread-detail"),
 
   # API URLs
 ]
