364a5019ff6c9ca65764e8644b747eab25477ed8
JeongSW123 <123189509+JeongSW123@users.noreply.github.com>
2026-05-03 23:16:27 +0900

    feat: 내정보 페이지, 회원정보 수정, 회원탈퇴 구현

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
   path("admin/", admin.site.urls),
   path("api/users/", include("accounts.urls")),
-  path("api/mypage/", include("mypage.urls")),
+  path("mypage/", include("mypage.urls")),
   path("chat/", include("chat.urls")),
   path("contract/", include("contract.urls")),
   path("", include("health.urls")),
@@ -20,7 +20,7 @@ urlpatterns = [
     name="page-password-reset",
   ),
   path("", TemplateView.as_view(template_name="index.html"), name="page-home"),
-  path("mypage/", TemplateView.as_view(template_name="mypage/mypage.html"), name="page-mypage"),
   # API URLs
   path("api/v1/", include("chat.api_urls")),
+  path("api/v1/", include("mypage.api_urls")),
 ]
